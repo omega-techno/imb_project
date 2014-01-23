3db2246b0c2d46f85d0cdea11ee21717695bc21b
vuthy32 <vuthy32@gmail.com>
2014-01-23 15:27:44 +0700

    Start

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php include('layers/head.html');?>
+
+<title>AECC Cambodia</title>
+</head>
+
+<body>
+
+
+	<?php include('layers/header_logo.html');?>
+	<?php include('layers/top_menu.html');?>
+	<!------------------------- MAIN CONTENT ------------------------------->
+	<div class="slide_outer">
+		
+		<div id="banner">
+      
+			<a href="images/main_slides/slider1.jpg"><img src="images/main_slides/slider1.jpg" id="img1"/></a>
+			<a href="images/main_slides/slider2.jpg"><img src="images/main_slides/slider2.jpg" id="img3"/></a>
+			<a href="images/main_slides/slider3.jpg"> <img src="images/main_slides/slider3.jpg" id="img2"/></a>	  
+			<a href="images/main_slides/slider4.jpg"> <img src="images/main_slides/slider4.jpg" id="img4"/></a>
+	   
+	   
+		</div>
+		<div class="screen_slide_text">
+			<div class="slide_text"><b>Climate Change and Food Security</b><br/><p>The farming population in Cambodia is predominantly smallholder and has not organized in an effective scale of crops or livestock and lack of standardization</p></div>
+		</div>
+	</div>
+	<div class="content_outer">
+		<div class="content screen">
+			
+			<div class="content_box">
+				<h3>Our e-Shop</h3>
+				<img src="images/contents/content1.jpg"/>
+				<p class="content_text">
+					Your partner for responding to challenges related to climate policies and emissions trading.
+				</p>
+				<div class="btn_goto_outer">
+					<a id="btn_content-1" class="btn_goto" href="#">Go to Homepage &#8594;</a>
+				</div>
+			</div>
+			<div class="content_box">
+				<h3>News and Updates</h3>
+				<img src="images/contents/content1.jpg"/>
+				<p class="content_text">
+					Your partner for responding to challenges related to climate policies and emissions trading.
+				</p>
+				<div class="btn_goto_outer">
+					<a id="btn_content-2" class="btn_goto" href="#">Go to Homepage &#8594;</a>
+				</div>
+			</div>
+			<div class="content_box">
+				<h3>What We Do</h3>
+				<img src="images/contents/content1.jpg"/>
+				<p class="content_text">
+					Your partner for responding to challenges related to climate policies and emissions trading.
+				</p>
+				<div class="btn_goto_outer">
+					<a id="btn_content-3" class="btn_goto" href="#">Go to Homepage &#8594;</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!------------------------- MAIN CONTENT ------------------------------->
+	<?php include('layers/footer.html');?>
+
---
+++ products.php
@@ -0,0 +1,76 @@
+<?php include('layers/head.html');?>
+
+<title>AECC Cambodia</title>
+<link rel="stylesheet" href="css/products.css">
+</head>
+
+<body>
+
+
+	<?php include('layers/header_logo.html');?>
+	<?php include('layers/top_menu.html');?>
+	<!------------------------- MAIN CONTENT ------------------------------->
+	<div class="slide_outer">
+		
+		<div id="banner">
+      
+			<a href="images/main_slides/slider1.jpg"><img src="images/main_slides/slider1.jpg" id="img1"/></a>
+			<a href="images/main_slides/slider2.jpg"><img src="images/main_slides/slider2.jpg" id="img3"/></a>
+			<a href="images/main_slides/slider3.jpg"> <img src="images/main_slides/slider3.jpg" id="img2"/></a>	  
+			<a href="images/main_slides/slider4.jpg"> <img src="images/main_slides/slider4.jpg" id="img4"/></a>
+	   
+	   
+		</div>
+		<div class="screen_slide_text">
+			<div class="slide_text"><b>Climate Change and Food Security</b><br/><p>The farming population in Cambodia is predominantly smallholder and has not organized in an effective scale of crops or livestock and lack of standardization</p></div>
+		</div>
+	</div>
+	<div class="content_outer">
+		<div class="content screen_content">
+			<div class="product_box">
+				<div class="stamp_add"></div>
+				<div class="product_img" >
+					<img src="images/products/thumb/product1.jpg" />
+					
+				</div>
+				<img class="new_stamp" src="images/new.png"/>
+				<div class="product_name_stamp">Product Name</div>
+				<p class="product_short_text">
+					Your partner for responding to challenges related to climate policies and emissions
+				</p>
+				<div class="price_label">$ 15<small>.00</small></div>
+				<div class="btn_detail"><a href="#">Detail</a></div>
+			</div>
+			<div class="product_box">
+				<div class="stamp_add"></div>
+				<div class="product_img" >
+					<img src="images/products/thumb/product1.jpg" />
+					
+				</div>
+				<img class="new_stamp" src="images/new.png"/>
+				<div class="product_name_stamp">Product Name</div>
+				<p class="product_short_text">
+					Your partner for responding to challenges related to climate policies and emissions
+				</p>
+				<div class="price_label">$ 15<small>.00</small></div>
+				<div class="btn_detail"><a href="#">Detail</a></div>
+			</div>
+			<div class="product_box">
+				<div class="stamp_add"></div>
+				<div class="product_img" >
+					<img src="images/products/thumb/product1.jpg" />
+					
+				</div>
+				<img class="new_stamp" src="images/new.png"/>
+				<div class="product_name_stamp">Product Name</div>
+				<p class="product_short_text">
+					Your partner for responding to challenges related to climate policies and emissions
+				</p>
+				<div class="price_label">$ 15<small>.00</small></div>
+				<div class="btn_detail"><a href="#">Detail</a></div>
+			</div>
+		</div>
+	</div>
+	<!------------------------- MAIN CONTENT ------------------------------->
+	<?php include('layers/footer.html');?>
+
